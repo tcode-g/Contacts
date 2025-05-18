81342586f0b76faf909d3621d4bb7768d64d13c3
Alfred Woodny Pierre <afredpw@gmail.com>
2025-05-17 20:08:47 -0400

    Update SignUp.php
    added some comments not final

--- LAMPAPI/SignUp.php
+++ LAMPAPI/SignUp.php
@@ -1,30 +1,30 @@
 
 <?php
 
-	$inData = getRequestInfo();
+	$inData = getRequestInfo();	// get input.
 	
 
 
-	$conn = new mysqli("localhost", "root", "", "myweb"); 	
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); // connect to database.	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT 1 FROM Users WHERE Login=?");
-		$stmt->bind_param("s", $inData["login"]);
+		$stmt = $conn->prepare("SELECT 1 FROM Users WHERE Login=?");	// mysql statement to search if user exists
+		$stmt->bind_param("s", $inData["login"]);	
 		$stmt->execute();
 		$result = $stmt->get_result();
 
-		if( mysqli_num_rows($result) < 1)
+		if( mysqli_num_rows($result) < 1) // new user
 		{
-			$istmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?) ");
-		    $istmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
+			$istmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?) ");	// mysql statement to add new user record.
+	    	$istmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
             $istmt->execute();
 		    $iresult = $istmt->get_result();
             if(mysqli_stmt_execute($istmt)){
-				returnwithInfo($inData["firstname"], $inData["lastname"], $inData["login"]);
+				returnwithInfo($inData["firstname"], $inData["lastname"], $inData["login"]);	// return with info of new added user.
             } else {
                 returnWithError(mysqli_stmt_error($istmt)); 
             }
@@ -32,7 +32,7 @@
 		}
 		else
 		{
-			returnWithError("User already exist");
+			returnWithError("User already exists");
 		}
 		
 		$stmt->close();
