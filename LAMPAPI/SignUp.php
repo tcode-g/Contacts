da48db5cc832e7a146f8c454e772cdefdab5f821
Alfred Woodny Pierre <afredpw@gmail.com>
2025-05-18 18:43:08 -0400

    Update SignUp.php

--- LAMPAPI/SignUp.php
+++ LAMPAPI/SignUp.php
@@ -1,29 +1,8 @@
 
 <?php
-	try{
-	$inData = getRequestInfo();	// get input.
 	
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); // connect to database.	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	} else {
-		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?) ");	// mysql statement to add new user record.
-		$stmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);	
-		$stmt->execute();
-		$result = $stmt->get_result();
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-	        if(mysqli_stmt_execute($stmt)){
-			returnwithInfo($inData["firstname"], $inData["lastname"], $inData["login"]);	// return with info of new added user.
-	        } else {
-			returnWithError(mysqli_stmt_error($stmt)); 
-	    	}
-		
-		
-		$stmt->close();
-		$conn->close();
-	}
-	
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
@@ -43,12 +22,37 @@
 	
 	function returnWithInfo( $firstName, $lastName, $slogin )
 	{ 
-		$retValue = '{"firstName":"' . $firstName . '","lastName":"' . $lastName . '","login":"' . $slogin . '","error":"","flag":0}';
+		$retValue = '{"firstName":"' . $firstName . '","lastName":"' . $lastName . '","lastName":"' . $slogin . '","error":"","flag":0}';
 		sendResultInfoAsJson( $retValue );
 	}
-	} catch(Exception $error) {
+
+	try{
+		
+		$inData = getRequestInfo();
+	
+		$conn = new mysqli("localhost", "root", "", "myweb"); 	
+		if( $conn->connect_error )
+		{
+			returnWithError( $conn->connect_error );
+		}
+		else
+		{
+			$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?) ");
+			$stmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
+			$stmt->execute();
+			returnwithInfo($inData["firstname"], $inData["lastname"], $inData["login"]);
+			$stmt->close();
+			$conn->close();
+		}
+	
+
+	} catch(mysqli_sql_exception $err){
+		if($err->getCode() == 1062){
+			$retValue = '{"firstName":"","lastName":"","lastName":"","error":"","flag":1}';
+		} else {
+			$retValue = '{"firstName":"","lastName":"","lastName":"","error":"","flag":2}';
+		}
 		header('Content-type: application/json');
-		$retValue = '{"error":"'.$error->getMessage().'"};
-  		echo $retValue;
+		echo $retValue;
 	}	
 ?>
