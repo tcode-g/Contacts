cca4f25cfd93a44f10e18e341044a522878c2fd6
Alfred Woodny Pierre <afredpw@gmail.com>
2025-05-29 19:50:10 -0400

    Update SearchContacts.php
    updated for pagination

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -3,6 +3,8 @@
 $inputData = getRequestInfo();
 
 $userId = $inputData['userid'];
+$limit = $inputData['limit'];
+$offset = $inputData['offset'];
 
 $search = isset($inputData['searchstring']) ? "%" . $inputData['searchstring'] . "%" : "%";
 
@@ -19,19 +21,34 @@ if ($conn->connect_error) {
 		Phone LIKE ? OR
 		Email LIKE ? OR
 		CONCAT(FirstName, ' ', LastName) LIKE ?
-	)");
-    $stmt->bind_param("isssss", $userId, $search, $search, $search, $search, $search);
+	) ORDER BY FirstName, LastName LIMIT ? OFFSET ? ");
+    $stmt->bind_param("isssssii", $userId, $search, $search, $search, $search, $search, $limit, $offset);
     $stmt->execute();
     $result = $stmt->get_result();
+
+    $stmtz = $conn->prepare("
+	SELECT COUNT(*) AS total_count FROM Contacts 
+	WHERE UserId = ? AND (
+		FirstName LIKE ? OR
+		LastName LIKE ? OR
+		Phone LIKE ? OR
+		Email LIKE ? OR
+		CONCAT(FirstName, ' ', LastName) LIKE ?
+	) ORDER BY FirstName, LastName LIMIT ? OFFSET ? ");
+    $stmtz->bind_param("isssssii", $userId, $search, $search, $search, $search, $search, $limit, $offset);
+    $stmtz->execute();
+    $resultz = $stmtz->get_result();
+	
     if ($result->num_rows > 0) {
 		$contacts = $result->fetch_all(MYSQLI_ASSOC);
-		$retValue = json_encode(["error" => false, "contacts" => $contacts]);
+		$total = $resultz->fetch_all(MYSQLI_ASSOC);
+		$retValue = json_encode(["error" => false, "contacts" => $contacts, "total" => $total]);
 		sendResultInfoAsJson($retValue);
 	} else {
 		$retValue = json_encode(["error"=> true, "error_message" => "No records found."]);
 		sendResultInfoAsJson($retValue);
 	}
-
+	
 	$stmt->close();
 	$conn->close();
 }
