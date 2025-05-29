79a2918b9160762e70f62da295b06ef311504e51
Alfred Woodny Pierre <afredpw@gmail.com>
2025-05-29 19:41:51 -0400

    Update GetContacts.php
    update for pagination

--- LAMPAPI/GetContacts.php
+++ LAMPAPI/GetContacts.php
@@ -3,19 +3,27 @@
 $inData = getRequestInfo();
 
 $userId = $inData["UserId"];
+$limit = $inData["limit"];
+$offset = $inData["offset"];
 
-$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+//$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+$conn = new mysqli("localhost", "root", "", "myweb");
 if ($conn->connect_error) {
 	returnWithError($conn->connect_error);
 } else {
-	$stmt = $conn->prepare("SELECT * from Contacts WHERE UserId = ?");
-	$stmt->bind_param("i", $userId);
+	$stmt = $conn->prepare("SELECT * from Contacts WHERE UserId = ? ORDER BY FirstName, LastName LIMIT ? OFFSET ?");
+	$stmt->bind_param("iii", $userId, $limit, $offset);
 	$stmt->execute();
 	$result = $stmt->get_result();
 
+	$stmtz = $conn->prepare("SELECT COUNT(*) AS total_count from Contacts WHERE UserId = ?");
+	$stmtz->bind_param("i", $userId);
+	$stmtz->execute();
+	$resultz = $stmtz->get_result();
 	if ($result->num_rows > 0) {
 		$contacts = $result->fetch_all(MYSQLI_ASSOC);
-		$retValue = json_encode(["error" => false, "contacts" => $contacts]);
+		$total = $resultz->fetch_all(MYSQLI_ASSOC);
+		$retValue = json_encode(["error" => false, "contacts" => $contacts, "total" => $total]);
 		sendResultInfoAsJson($retValue);
 	} else {
 		$retValue = json_encode(["error"=> true, "error_message" => "No records found."]);
