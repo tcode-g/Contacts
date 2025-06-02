9309601ec6e656e3831ddfe320980cfab11602d7
Alfred Woodny Pierre <afredpw@gmail.com>
2025-06-02 08:56:38 -0400

    Update check.js

--- js/check.js
+++ js/check.js
@@ -3,6 +3,8 @@ let firstNameIsValid = false;
 let lastNameIsValid = false;
 let userNameIsValid = false;
 let passwordIsValid = false;
+let phoneIsValid = false;
+let emailIsValid = false;
 
 function validateName(id) 
 {
@@ -16,13 +18,13 @@ function validateName(id)
         {
             let char = element.value.charAt(0);
             element.value = element.value.replace(char, char.toUpperCase());
-            if(id == "signupFirstName"){
+            if(id == "signupFirstName" || id == "first" || id == "iData1"){
                 firstNameIsValid = true;
             } else {
                 lastNameIsValid = true;
             }
         } else if(currentLength > 30) {
-            if(id == "signupFirstName"){
+            if(id == "signupFirstName" || id == "first"){
                 firstNameIsValid = false;
             } else {
                 lastNameIsValid = false;
@@ -32,13 +34,16 @@ function validateName(id)
             element.value = tempStr;
             if(id == "signupFirstName"){
                 document.getElementById("signUpFnameMsg").innerHTML = "max char count";
-            } else {
+            } else if(id == "signupLastName")  {
                 document.getElementById("signUpLnameMsg").innerHtml = "max char count";
             }
         } else {
-            document.getElementById("signUpFnameMsg").innerHTML = "";
-            document.getElementById("signUpLnameMsg").innerHTML = "";
             if(id == "signupFirstName"){
+                document.getElementById("signUpFnameMsg").innerHTML = "";
+            } else if(id == "signupLastName")  {
+                document.getElementById("signUpLnameMsg").innerHtml = "";
+            }
+            if(id == "signupFirstName" || id == "first" || id == "iData1"){
                 firstNameIsValid = true;
             } else {
                 lastNameIsValid = true;
@@ -152,6 +157,82 @@ function checkUsername(){
 	}
 }
 
+
+function validatePhone(id)
+{
+    let element = document.getElementById(id);
+    element.value = element.value.replace(/[^0-9-]/g, "");
+    let currentLength = element.value.length;
+    let tempStr = element.value;
+    tempStr = tempStr.replace(/[^0-9]/g, "");
+    let numLength = tempStr.length;
+    console.log(currentLength + " " + numLength);
+    if(numLength < 10){
+        phoneIsValid = false;
+        if(element.value.charAt(currentLength - 1) == "-"){
+            if(currentLength == 4 || currentLength == 8){
+                // do nothing
+            } else {
+                let tempStr = element.value.slice(0, -1);
+                element.value = element.value.replace(/./g, "");
+                element.value = tempStr;
+            }
+        }
+    } else if(numLength > 10){
+        phoneIsValid = false;
+        let tempStr = element.value.slice(0, -1);
+        element.value = element.value.replace(/./g, "");
+        element.value = tempStr;
+    } else {
+        phoneIsValid = true;
+    }
+}
+
+function sanitizePhone(id)
+{
+    if(!phoneIsValid){
+        return;
+    }
+
+    let element = document.getElementById(id);
+    element.value = element.value.replace(/[^0-9]/g, "");
+}
+
+function displayPhone(str)
+{
+    let index = 0;
+    str = str.replace(/[^0-9]/g, "");
+    if(str.length != 10){
+        return str;
+    }
+    let baseStr = "000-000-0000".split('');
+    for(let x = 0; x < 12; x++){
+        if(x == 3 || x == 7){
+            // do nothing.
+        } else {
+            baseStr[x] = str.charAt(index++);
+        }
+    }
+    return baseStr.join("");
+}
+
+function validateEmail(id)
+{
+    let element = document.getElementById(id);
+    element.value = element.value.replace(/[ ]/g, "");
+    let currentLength = element.value.length;
+    if(currentLength < 3){
+        emailIsValid = false;
+    } else {
+        if(/[a-zA-Z0-9.!#$%^&*_?=]+@[a-zA-Z0-9-.]+\.[a-zA-Z]{2,}/g.test(element.value)){
+            emailIsValid = true;
+        } else {
+            emailIsValid = false;
+        }
+    }
+}
+
+
 /** 
  * Checks that all form fields have acceptable content in them
  * If any isValid has a non-truthy value, it will return false
@@ -159,4 +240,62 @@ function checkUsername(){
  */
 function checkSignUpForm() {
     return firstNameIsValid && lastNameIsValid && userNameIsValid && passwordIsValid;
+}
+
+function checkContactForm() {
+    if(!firstNameIsValid){
+        document.getElementById("first").classList.add("invalid");
+    } else {
+       document.getElementById("first").classList.remove("invalid"); 
+    }
+    if(!lastNameIsValid){
+        document.getElementById("last").classList.add("invalid");
+    } else {
+        document.getElementById("last").classList.remove("invalid"); 
+    }
+    if(!emailIsValid){
+        document.getElementById("email").classList.add("invalid");
+    } else {
+       document.getElementById("email").classList.remove("invalid"); 
+    }
+    if(!phoneIsValid){
+        document.getElementById("phone").classList.add("invalid");
+    } else {
+        sanitizePhone("phone");
+        document.getElementById("phone").classList.remove("invalid"); 
+    }
+    return firstNameIsValid && lastNameIsValid && phoneIsValid && emailIsValid;
+}
+
+function checkEditContact()
+{
+    validateName("iData1");
+    validateName("iData2");
+    validatePhone("iData3");
+    validateEmail("iData4");
+        
+    
+    if(!firstNameIsValid){
+        document.getElementById("iData1").classList.add("invalid");
+    } else {
+       document.getElementById("iData1").classList.remove("invalid"); 
+    }
+    if(!lastNameIsValid){
+        document.getElementById("iData2").classList.add("invalid");
+    } else {
+        document.getElementById("iData2").classList.remove("invalid"); 
+    }
+    if(!emailIsValid){
+        document.getElementById("iData4").classList.add("invalid");
+    } else {
+       document.getElementById("iData4").classList.remove("invalid"); 
+    }
+    if(!phoneIsValid){
+        document.getElementById("iData3").classList.add("invalid");
+    } else {
+        sanitizePhone("iData3");
+        document.getElementById("iData3").classList.remove("invalid"); 
+    }
+    console.log(firstNameIsValid + " " + lastNameIsValid + " " + emailIsValid + " " + phoneIsValid);
+    return firstNameIsValid && lastNameIsValid && phoneIsValid && emailIsValid;
 }
